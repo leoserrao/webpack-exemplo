bf756a9ff2c3dc7955eefe2146994271e8ec701c
Léo Serrão Barbosa <leo.barbosa@ifap.edu.br>
2022-04-08 21:24:50 -0300

    Criando html dinamicamente e estilizando com webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import soma from './calc.js';
+import Heading from './components/heading.js';
 
 console.log("Testando o webpack 2");
 
 soma(3, 8);
 soma(5, 10);
+
+//Loaders
+const heading = new Heading();
+
+heading.create("Este título é dinâmico");
